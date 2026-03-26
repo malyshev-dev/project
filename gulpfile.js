622266d24e87f676e261767b449c49fe896ea1c0
AlexeyM <maom7@mail.ru>
2026-03-26 21:14:39 +0400

    install gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 
 gulp.task('server', function() {
-
     browserSync({
         server: {
             baseDir: "src"
@@ -17,17 +16,17 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({ cascade: false }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
